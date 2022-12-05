40d573320e77aa84b3ed123ca1f9e314ee9a895a
Vianne <32017284+shaovatwit@users.noreply.github.com>
2022-12-05 11:13:10 -0500

    Managed to get the regex

--- password_checker_storage.sh
+++ password_checker_storage.sh
@@ -2,7 +2,7 @@
 db_user="postgres"
 db_hostname="localhost"
 db_database="info_db"
-db_password="password"
+# db_password="password"
 
 echo "Username OR Email: "
 read user
@@ -11,15 +11,23 @@ echo "Password: "
 read password
 
 #regex goes here, also be sure to check this field is NOT empty.
+if [[ ${#password} -ge 8 && "$password" == *[A-Z]* && "$password" == *[a-z]* && "$password" == *[0-9]* && "$password" == *['!@#$%&*^']* ]]
+then
 
 #creates extension for encrypting password if not exists.
 # psql -U $db_user -h $db_hostname -d $db_database -c  "CREATE EXTENSION IF NOT EXISTS pgcrypto"
 
-#create table if not exists
+#create table if not exists in tables.sql file
 
+#for pgpass file, host:port:db:user:pass
+#chmod 600 pgpass
+#export PGPASSFILE='/Users/vianne/Documents/GitHub/password-storage/.pgpass' - environmental variable
 
 #encrypts password and stores into database
 psql -U $db_user -h $db_hostname -d $db_database -c "INSERT INTO user_info (email, password) VALUES ('$user', crypt('$password', gen_salt('bf')))"
-
+echo "Successfully inserted information into database."
 #documentation used
 #https://www.postgresql.org/docs/current/pgcrypto.html
+else
+printf "Invalid password!\n- At least 8 characters\n- At least one uppercase letter\n- At least one lowercase letter\n- At least one special character\n- At least one number\n"
+fi
